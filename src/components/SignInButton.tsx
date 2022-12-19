16589fd7fe32dcfb94903fcba9718d78d3fc18a2
Roy <rhavila789@gmail.com>
2022-12-19 18:38:57 -0500

    feat: auth0

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,6 +1,14 @@
+import { useAuth0 } from '@auth0/auth0-react'
+
 const SignInButton = () => {
+    const { loginWithRedirect } = useAuth0()
+
+    const handleLogin = () => {
+        loginWithRedirect()
+    }
+
     return (
-        <button className="btn rounded-pill d-flex gap-2 align-items-center border px-3" title="Sign In">
+        <button onClick={handleLogin} className="btn rounded-pill d-flex gap-2 align-items-center border px-3" title="Sign In">
             <img src="icons/LoginUserIcon.svg" alt="" />
             <p className="text-primary m-0">Sign In</p>
         </button>
--- src/pages/Subscribes.tsx
+++ src/pages/Subscribes.tsx
@@ -1,12 +1,18 @@
 import SignInButton from "../components/SignInButton"
+import { useAuth0 } from '@auth0/auth0-react'
 
 const Subscribes = () => {
+    const { isAuthenticated } = useAuth0();
     return (
-        <div className="d-flex position-absolute justify-content-center align-items-center w-100 h-100 flex-column">
-            <img className="pb-5" src="icons/SubsOutline.svg" width='120px' height='120px' alt="" />
-            <p className="fs-4">Don’t miss new videos</p>
-            <p>Sign in to see updates from your favorite YouTube channels</p>
-            <SignInButton />
+        <div className="subscribe">
+            {
+                isAuthenticated ? <div>Logged</div> : <div className="d-flex position-absolute justify-content-center align-items-center w-100 h-100 flex-column">
+                    <img className="pb-5" src="icons/SubsOutline.svg" width='120px' height='120px' alt="" />
+                    <p className="fs-4">Don’t miss new videos</p>
+                    <p>Sign in to see updates from your favorite YouTube channels</p>
+                    <SignInButton />
+                </div>
+            }
         </div>
     )
 }
