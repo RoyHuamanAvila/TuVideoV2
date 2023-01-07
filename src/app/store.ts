7451e2c1a37195031ab7962117d73886c820deb6
Roy <rhavila789@gmail.com>
2023-01-06 20:50:40 -0500

    feat: create channel and fix minor errors

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import channelReducer from "../features/channel/channelSlice";
+import userReducer from "../features/user/userSlice";
 
 export const store = configureStore({
-    reducer: {
-        yourChannel: channelReducer
-    }
-})
+  reducer: {
+    yourChannel: channelReducer,
+    user: userReducer,
+  },
+});
 
-export type RootState = ReturnType<typeof store.getState>
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,9 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export interface Icon {
-    color?: string;
+  color?: string;
 }
 
-export interface MyChannel {
-    active: boolean;
-    name: string;
-    logo: string;
+export interface ThunkDataTransfer {
+  data: any;
+  token: string;
+}
+
+export interface CreateChannel {
+  name: string;
+  logo: string;
+}
+
+export interface Channel {
+  name: string;
+  logo: string;
+  banner: string;
+  owner: string;
+}
+
+export interface UserData {
+  created_at: Date;
+  email: string;
+  identities: Identity[];
+  name: string;
+  nickname: string;
+  picture: string;
+  updated_at: Date;
+  user_id: string;
+  user_metadata: UserMetadata;
+  last_ip: string;
+  last_login: Date;
+  logins_count: number;
+}
+
+export interface Identity {
+  connection: string;
+  provider: string;
+  user_id: string;
+  isSocial: boolean;
+}
+
+export interface UserMetadata {
+  channel: string;
 }
