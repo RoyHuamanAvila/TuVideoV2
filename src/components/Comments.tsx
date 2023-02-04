3ace41093393495d01470eb6820830e9c7041a8a
Roy <rhavila789@gmail.com>
2023-02-03 23:17:10 -0500

    feat: comment crud

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,19 +1,97 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { FC, useEffect, useState } from "react"
-import { AppDispatch, RootState } from "../app/store";
+import { FC, useEffect, useState } from "react"
 import axios from "axios";
-import { Comment } from "../interfaces";
-import { useDispatch, useSelector } from "react-redux";
-import { comment } from "../features/user/userSlice";
-import { toast } from "react-toastify";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Comment } from "../interfaces";
+import BoxAddComment from "./Comment/BoxAddComment";
+import BoxComment from "./Comment/BoxComment";
+import { toast } from "react-toastify";
 
 const Comments: FC<{ videoID: string }> = ({ videoID }) => {
     const [comments, setComments] = useState<Comment[]>([]);
     const { user } = useAuth0();
+    const DOMAIN_BD = import.meta.env.VITE_DOMAIN_BD;
+    const token = localStorage.getItem('token');
+
+    const handleDeleteComment = async (commentID: string) => {
+        if (confirm('Delete comment?')) {
+            try {
+                await axios.request({
+                    url: `${DOMAIN_BD}/comment/${commentID}`,
+                    method: 'DELETE',
+                    headers: {
+                        authorization: `Bearer ${token}`,
+                        'content-type': 'application/json',
+                    },
+                })
+                setComments(comments.filter(comment => comment._id !== commentID));
+                toast('Delete comment successful', {
+                    type: "success"
+                });
+            } catch (error) {
+                toast('Error to delete', {
+                    type: "error"
+                });
+                console.log(error);
+            }
+        }
+    }
+
+    const handleAddComment = async (content: string, videoID: string) => {
+        try {
+            const response = await axios.request({
+                url: `${DOMAIN_BD}/comment/${videoID}`,
+                method: 'POST',
+                headers: {
+                    authorization: `Bearer ${token}`,
+                    'content-type': 'application/json',
+                },
+                data: { content }
+            })
+            toast('Add comment successful', {
+                type: "success"
+            });
+            setComments(prevComments => prevComments.concat(response.data.createdComment));
+        } catch (error) {
+            console.log(error);
+            toast('Error to comment', {
+                type: "error"
+            });
+        }
+    }
+
+    const handleUpdateComment = async (newContent: string, _id: string) => {
+        try {
+            await axios.request({
+                url: `${DOMAIN_BD}/comment/${_id}`,
+                method: 'PATCH',
+                headers: {
+                    authorization: `Bearer ${token}`,
+                    'content-type': 'application/json',
+                },
+                data: { content: newContent }
+            })
+
+            setComments(prevComments => prevComments.map(comment => {
+                if (comment._id === _id) return { ...comment, content: newContent }
+                return comment;
+            }))
+
+            toast('Update comment successful', {
+                type: "success"
+            });
+
+        } catch (error) {
+            toast('Error to update comment', {
+                type: "error"
+            });
+            console.log(error);
+        }
+    }
 
     const getComments = async () => {
         try {
+            setComments([]);
             const response = await axios.get(`${import.meta.env.VITE_DOMAIN_BD}/video/${videoID}/comments`);
             setComments(response.data.comments);
         } catch (error) {
@@ -29,85 +107,27 @@ const Comments: FC<{ videoID: string }> = ({ videoID }) => {
         <div className="pt-4 pb-3 border-bottom">
             <p>{comments.length} comments</p>
             {
-                user && <BoxAddComment videoID={videoID} setComments={setComments} />
+                user &&
+                <BoxAddComment
+                    videoID={videoID}
+                    addComment={handleAddComment}
+                />
             }
             <div>
                 {
-                    comments.map((comment, index) => <BoxComment key={index} {...comment} />)
+                    comments.map((comment, index) => (
+                        <BoxComment
+                            key={index}
+                            comment={comment}
+                            updateComment={handleUpdateComment}
+                            deleteComment={handleDeleteComment}
+                        />
+                    ))
                 }
             </div>
         </div>
     )
 }
 
-const BoxAddComment: FC<{ videoID: string, setComments: React.Dispatch<any> }> = ({ videoID, setComments }) => {
-    const [active, setActive] = useState(false);
-    const [content, setContent] = useState<string>();
-    const userAuth0 = useSelector((state: RootState) => state.user);
-    const handleActive = () => setActive(true);
-    const handleDesactive = () => setActive(false);
-    const dispatch = useDispatch<AppDispatch>();
-    const token = localStorage.getItem('token');
 
-    const handleComment = async () => {
-        if (content && token) {
-            dispatch(comment({ content, token, videoID })).then((fullfiled) => {
-                toast('Comment created', {
-                    type: "success"
-                })
-                setComments((prevComments: any) => [...prevComments, fullfiled.payload.createdComment])
-            }).catch((error) => {
-                console.log(error);
-                toast('Error to create comment', {
-                    type: "error"
-                })
-            })
-            setContent('');
-            setActive(false);
-        }
-    }
-
-    return (
-        <div className="d-flex align-items-flexstart gap-3 mb-4">
-            <div className="channel-logo--md overflow-hidden">
-                <img className='channel-logo-img' src={userAuth0?.picture} alt="user picture" />
-            </div>
-            <div className="w-100 d-flex flex-column">
-                <div className="comment-input-control mb-2">
-                    <input className="comment-input" type="text" placeholder="Enter your comment" value={content} onChange={(e) => setContent(e.currentTarget.value)} required onFocus={handleActive} />
-                </div>
-                {
-                    active && (
-                        <div className="align-self-end d-flex gap-2">
-                            <button className="border-0 bg-transparent text-light" onClick={handleDesactive}>CANCEL</button>
-                            <button className="btn btn-secondary rounded-0 text-white" onClick={handleComment}>COMMENT</button>
-                        </div>
-                    )
-                }
-            </div>
-        </div>
-    )
-}
-
-const BoxComment = ({ content, owner }: Comment) => {
-    return (
-        <div className="d-flex align-flex-start gap-3">
-            <div className="channel-logo--md overflow-hidden">
-                <img className="channel-logo-img" src={owner.logo as string} alt="logo" />
-            </div>
-            <div className="w-100">
-                <div className="d-flex justify-content-between">
-                    <p className="comment-title">{owner.name} <span className="fw-normal text-light">hace 15 minutos</span></p>
-                    <div>
-                        <button className="bi bi-pencil border-0 bg-transparent" title="Edit"></button>
-                        <button className="bi bi-trash3 border-0 bg-transparent" title="Delete"></button>
-                    </div>
-                </div>
-                <p className="comment-text">
-                    {content}
-                </p>
-            </div>
-        </div>
-    )
-}
 export default Comments
