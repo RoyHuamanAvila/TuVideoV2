b291b36e4b2f1ca957d96c380ab7fc9df8224444
Roy Huaman <RHavila789@gmail.com>
2023-09-06 21:36:27 -0500

    feat: responsive header

--- src/components/Header/HeaderView.tsx
+++ src/components/Header/HeaderView.tsx
@@ -4,9 +4,12 @@ import './Header.scss';
 const HeaderView = () => {
   return (
     <header className='px-3 d-flex align-items-center justify-content-between bg-background'>
-      <Link to='/'>
-        <img src="/icons/TuVideoIcon.svg" alt="Logo TuVideo" />
-      </Link>
+      <div className="d-flex align-items-center">
+        <i className="bi bi-list fs-4 px-2 text-grey-600 d-none d-md-block"></i>
+        <Link to='/'>
+          <img className='px-md-3' src="/icons/TuVideoIcon.svg" alt="Logo TuVideo" />
+        </Link>
+      </div>
       <img src="/icons/icon-profile-circled.svg" alt="Icon Profile" />
     </header>
   )
